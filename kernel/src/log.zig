10a03e42d3e1c77f53af1fff17f3254e87be9165
Ryder Retzlaff <ryder@retzlaff.family>
2025-09-17 22:22:19 +1000

    Migrate log.zig to zig 0.15.1

--- kernel/src/log.zig
+++ kernel/src/log.zig
@@ -5,22 +5,31 @@ const Serial = @import("serial.zig").Serial;
 const LoggingError = error{};
 
 // const Writer = std.io.Writer(void, LoggingError, logCallback);
-// var log_buffer: []u8 = {};
-// var log_writer = std.Io.Writer{ .buffer = log_buffer, .end = 0, .vtable = .{ .drain = logCallback } };
+var log_buffer: [1]u8 = .{0};
+var log_writer = std.Io.Writer{ .buffer = &log_buffer, .end = 0, .vtable = &.{ .drain = logCallback } };
 
 var serial: Serial = undefined;
 
-// fn logCallback(w: *std.Io.Writer) LoggingError!usize {
-//     serial.writeBytes(w.buffer[0..w.end]);
-//     return w.end;
-// }
+fn logCallback(w: *std.Io.Writer, data: []const []const u8, splat: usize) LoggingError!usize {
+    const buffered = w.buffered();
+    if (buffered.len != 0) {
+        serial.writeBytes(buffered);
+        return w.consume(buffered.len);
+    }
+    for (data[0 .. data.len - 1]) |buf| {
+        if (buf.len == 0) continue;
+        serial.writeBytes(buf);
+        return w.consume(buf.len);
+    }
+    const pattern = data[data.len - 1];
+    if (pattern.len == 0 or splat == 0) return 0;
+    serial.writeBytes(pattern);
+    return w.consume(pattern.len);
+}
 
 pub fn log(comptime level: std.log.Level, comptime format: []const u8, args: anytype) void {
     // fmt.format(Writer{ .context = {} }, "[" ++ @tagName(level) ++ "]" ++ format ++ "\n", args) catch unreachable;
-    // log_writer.print("[{}]");
-    _ = level;
-    _ = format;
-    _ = args;
+    log_writer.print("[{s}] " ++ format ++ "\n", .{@tagName(level)} ++ args) catch unreachable;
 }
 
 pub fn init(ser: Serial) void {
